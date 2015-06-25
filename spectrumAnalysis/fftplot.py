76d9de160b8578a75acaa1f7975e75d86774f26c
ntlhui <ntlhui@gmail.com>
2015-06-25 11:41:59 -0700

    Added fftplotter

---
+++ spectrumAnalysis/fftplot.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plot
+from mpl_toolkits.mplot3d import axes3d
+
+data = np.genfromtxt("/home/e4e/fiona/fft1.txt", delimiter=',')
+
+# get first row
+
+X = data[0][1:]
+
+Z = [(row[1:]) for row in data[1:]]
+Y = [row[0] for row in data[1:]]
+
+X, Y = np.meshgrid(X, Y)
+
+
+fig = plot.figure()
+ax = fig.gca(projection='3d')
+
+ax.plot_surface(X, Y, np.array(Z))
+
+plot.savefig("fftout.png", bbox_inches='tight')
