774325cce2748e1cfa85357268d19847c2812eee
ntlhui <ntlhui@gmail.com>
2015-06-25 17:35:46 -0700

    Added code to utilize a sequence of files

--- spectrumAnalysis/fftplot.py
+++ spectrumAnalysis/fftplot.py
@@ -5,7 +5,7 @@ mpl.use('Agg')
 import matplotlib.pyplot as plot
 from mpl_toolkits.mplot3d import axes3d
 
-data = np.genfromtxt("/home/e4e/fiona/fft1.txt", delimiter=',')
+data = np.genfromtxt("/home/e4e/fiona/output00.txt", delimiter=',')
 
 # get first row
 
@@ -14,8 +14,13 @@ X = data[0][1:]
 Z = [(row[1:]) for row in data[1:]]
 Y = [row[0] for row in data[1:]]
 
-X, Y = np.meshgrid(X, Y)
+for i in range(20):
+	data = np.genfromtxt("/home/e4e/fiona/output%2d" % i, delimiter=',')
+	for row in data:
+		Y.append(row[0])
+		Z.append(row[1:])
 
+X, Y = np.meshgrid(X, Y)
 
 fig = plot.figure()
 ax = fig.gca(projection='3d')
