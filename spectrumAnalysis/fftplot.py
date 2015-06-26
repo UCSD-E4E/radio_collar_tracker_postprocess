21dc9027e281bb81bff1124c5a2707c6ee3a759d
ntlhui <ntlhui@gmail.com>
2015-06-26 15:25:50 -0700

    Fixed list comprehensions for plotting data

--- spectrumAnalysis/fftplot.py
+++ spectrumAnalysis/fftplot.py
@@ -5,7 +5,7 @@ mpl.use('Agg')
 import matplotlib.pyplot as plot
 from mpl_toolkits.mplot3d import axes3d
 
-X = np.genfromtxt("fftheader.txt", delimiter=',', skip_footer=3)
+X_labels = np.genfromtxt("fftheader.txt", delimiter=',', skip_footer=3)
 data = np.genfromtxt("fftheader.txt", delimiter=',', skip_header=1)
 minFFT = data[1]
 maxFFT = data[2]
@@ -14,7 +14,9 @@ numFiles = data[0]
 
 for i in range(int(numFiles)):
 	data = np.genfromtxt("output%02d.txt"%(i), delimiter=',')
-	Y = [row[0] for row in data]
+	Y_labels = [row[0] for row in data]
 	Z = [(row[1:]) for row in data]
+	X = [X_labels(x) for x in range(len(X_labels)) for y in range(len(Y_labels))]
+	Y = [Y_labels(y) for x in range(len(X_labels)) for y in range(len(Y_labels))]
 	sc = plot.scatter(X, Y, Z, vmin = minFFT, vmax = maxFFT)
 	plot.savefig("output%02d.png"%(i), bbox_inches='tight')
