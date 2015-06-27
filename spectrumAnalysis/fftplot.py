ee5080598f074ab2feb1720f23496ef095ccf777
ntlhui <ntlhui@gmail.com>
2015-06-27 00:19:11 -0700

    Added multiprocessing capability

--- spectrumAnalysis/fftplot.py
+++ spectrumAnalysis/fftplot.py
@@ -3,19 +3,9 @@ import numpy as np
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plot
+from multiprocessing import Pool
 
-X_labels = np.genfromtxt("fftheader.txt", delimiter=',', skip_footer=3)
-maxFreq = np.amax(X_labels)
-minFreq = np.amin(X_labels)
-data = np.genfromtxt("fftheader.txt", skip_header=1)
-minFFT = data[1]
-maxFFT = data[2]
-numFiles = data[0]
-# get first row
-
-print("Got header\n")
-
-for i in range(int(numFiles)):
+def procFile(i):
 	print("Getting data from file %d..."%(i))
 	outputfile = open("output%02d.txt"%(i), "r")
 	print("done")
@@ -42,3 +32,19 @@ for i in range(int(numFiles)):
 		plot.savefig("output_%010d.png"%(int(time)), bbox_inches='tight')
 		# break
 	outputfile.close()
+	return
+
+X_labels = np.genfromtxt("fftheader.txt", delimiter=',', skip_footer=3)
+maxFreq = np.amax(X_labels)
+minFreq = np.amin(X_labels)
+data = np.genfromtxt("fftheader.txt", skip_header=1)
+minFFT = data[1]
+maxFFT = data[2]
+numFiles = data[0]
+# get first row
+
+print("Got header\n")
+
+p = Pool(8)
+
+p.map(procFile, range(int(numFiles)))
