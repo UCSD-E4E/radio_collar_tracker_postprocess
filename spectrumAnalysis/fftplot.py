63b0d854e75b2294f93a29133c9b0c20bc748e95
ntlhui <ntlhui@gmail.com>
2015-06-26 17:07:12 -0700

    Single fft mode

--- spectrumAnalysis/fftplot.py
+++ spectrumAnalysis/fftplot.py
@@ -3,29 +3,34 @@ import numpy as np
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plot
-from mpl_toolkits.mplot3d import axes3d
 
 X_labels = np.genfromtxt("fftheader.txt", delimiter=',', skip_footer=3)
-data = np.genfromtxt("fftheader.txt", delimiter=',', skip_header=1)
+maxFreq = np.amax(X_labels)
+minFreq = np.amin(X_labels)
+data = np.genfromtxt("fftheader.txt", skip_header=1)
 minFFT = data[1]
 maxFFT = data[2]
-numFiles = 56
+numFiles = data[0]
 # get first row
 
 print("Got header\n")
 
-# for i in range(int(numFiles)):
-for i in range(1):
+for i in range(int(numFiles)):
+# for i in range(1):
 	print("Getting data from file %d..."%(i))
 	outputfile = open("test%02d.txt"%(i), "r")
 	print("done\n")
-	Y_labels = [line.strip().split(',')[0] for line in outputfile]
-	Z = [float(i) for i in [line.strip().split(',')[1:] for line in outputfile]]
-	print("Converting axes...")
-	X = [X_labels[x] for x in range(len(X_labels)) for y in range(len(Y_labels))]
-	Y = [Y_labels[y] for x in range(len(X_labels)) for y in range(len(Y_labels))]
-	print("done\n")
-	print("Plotting...")
-	sc = plot.scatter(X, Y, Z, vmin = minFFT, vmax = maxFFT)
-	print("done\n")
-	plot.savefig("output%02d.png"%(i), bbox_inches='tight')
+	for line in outputfile:
+		print("Getting line...")
+		time = line.strip().split(',')[0]
+		fft = [float(data) for data in line.strip().split(',')[1:]]
+		print("done\n")
+
+		print("Plotting...")
+		plt = plot.plot(X_labels, fft)
+		ax = plot.gca()
+		ax.set_xlim(left=minFFT, right=maxFFT)
+		ax.set_title("Time: %8d"%time)
+		plot.xticks(rotation='vertical')
+		plot.savefig("output%08d.png"%(time))
+	outputfile.close()
