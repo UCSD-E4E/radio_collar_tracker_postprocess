c5461d6937650e76fd478aa9919278e173e406d6
ntlhui <ntlhui@gmail.com>
2015-06-29 08:58:18 -0700

    Fixed string conversion

--- spectrumAnalysis/frequency_extract.py
+++ spectrumAnalysis/frequency_extract.py
@@ -39,7 +39,7 @@ args = parser.parse_args()
 X_labels = np.genfromtxt("fftheader.txt", delimiter=',', skip_footer=3)
 X_label = [float(label) for label in X_labels]
 
-freq_index = args.frequency
+freq_index = int(args.frequency)
 print("Using frequency %.3f" % (X_label[freq_index]))
 target_frequency = X_label[freq_index]
 data = np.genfromtxt("fftheader.txt", skip_header=1)
