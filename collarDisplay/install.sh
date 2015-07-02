ec85e2eb7640b70da163452c64e89a413a4e04fe
ntlhui <ntlhui@gmail.com>
2015-07-02 11:45:44 -0700

    Fixed install path

--- collarDisplay/install.sh
+++ collarDisplay/install.sh
@@ -2,7 +2,7 @@
 mkdir $HOME/rct $HOME/rct/bin $HOME/rct/lib
 COL_DISP_DIR=$(pwd)
 sudo pip install -e git+git://github.com/Turbo87/utm.git#egg=master --src $HOME/rct/lib/
-cp ${COL_DISP_DIR}/display_data.py $HOME/rct/bin/
+cp ${COL_DISP_DIR}/collarDisplay/display_data.py $HOME/rct/bin/
 chmod u+x $HOME/rct/bin/display_data.py
 if [ ! -e "$HOME/.bashrc" ] ; then
 	echo '#!/bin/bash' >> $HOME/.bashrc
