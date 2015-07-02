4a2f34396493dc3a3160825b29cff46e849cb998
ntlhui <ntlhui@gmail.com>
2015-07-02 11:35:59 -0700

    Fixed install directories

--- make_bin.sh
+++ make_bin.sh
@@ -1,22 +1,14 @@
-if [ -e "bin" ]
-	then
-	echo ""
-	else
-		mkdir bin
-	fi
-cd CurrentCode/PostProcessC/
+REPO_DIR=$(pwd)
+if [ ! -d "$HOME/rct/bin/" ] ; then
+	mkdir -p $HOME/rct/bin/
+fi
+cd $(REPO_DIR)/CurrentCode/PostProcessC/
 chmod +x build*
-./build-finalAnalysis
 ./build-spectrumAnalysis
-cd ../../collarDetect
-make all
-cd ../bin
-cp ../collarDetect/addNoiseCh .
-cp ../collarDetect/altFilter.py .
-cp ../CurrentCode/PostProcessC/COL .
-cp ../CurrentCode/PostProcessC/finalAnalysis .
-cp ../collarDetect/spectraCollarID .
-cp ../CurrentCode/PostProcessC/spectrumAnalysis .
-cp ../CLI_GUI/runcli.sh .
-tar -cf run.tar addNoiseCh altFilter.py COL finalAnalysis spectraCollarID spectrumAnalysis run2.sh runcli.sh run.sh
-cd ..
+
+cd $(REPO_DIR)
+cp $(REPO_DIR)/collarDetect/altFilter.py $HOME/rct/bin/
+cp $(REPO_DIR)/CurrentCode/PostProcessC/spectrumAnalysis $HOME/rct/bin/
+cp $(REPO_DIR)/CLI_GUI/cas.sh $HOME/rct/bin/
+cp $(REPO_DIR)/CLI_GUI/get_run_num.py $HOME/rct/bin/
+chmod +x $HOME/rct/bin/*
