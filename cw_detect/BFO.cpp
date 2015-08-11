67d799b8d08608869a7ee2fb865b99870e564b93
ntlhui <ntlhui@gmail.com>
2015-08-11 11:01:35 -0700

    Fixed complex definition prototypes, fixed complex declaration, fixed queue type

--- cw_detect/BFO.cpp
+++ cw_detect/BFO.cpp
@@ -6,48 +6,55 @@
 #include "Sample.hpp"
 #include <queue>
 
+#ifndef QUEUE_SIZE_MAX
+#define QUEUE_SIZE_MAX 1024000
+#endif // QUEUE_SIZE_MAX
+
 using namespace std;
 
-BFO::BFO(int sample_rate, int frequency, CRFSample (*out_queue)()):sample_rate(sample_rate), OSC_freq(frequency), run_state(true){
+BFO::BFO(int sample_rate, int frequency,
+         CRFSample * (*out_queue)()): sample_rate(sample_rate), OSC_freq(frequency),
+	run_state(true) {
 	next_sample = out_queue;
 	// Start worker thread associated with this class
 	thread class_thread(&BFO::run, this);
 }
 
-BFO::~BFO(){
+BFO::~BFO() {
 	run_state = false;
 	class_thread.join();
 }
 
-CRFSample* BFO::getNextSample(){
-	if(output_queue.empty()){
+CRFSample* BFO::getNextSample() {
+	if (output_queue.empty()) {
 		return NULL;
 	}
 	output_queue_mutex.lock();
-	CRFSample retval = output_queue.front();
+	CRFSample* retval = output_queue.front();
 	output_queue.pop();
 	output_queue_mutex.unlock();
-	return &retval;
+	return retval;
 }
 
-void BFO::run(){
-	while(run_state){
+void BFO::run() {
+	while (run_state) {
 		// Get next sample
-		CRFSample sample = next_sample();
-		if(!sample){
+		CRFSample* sample = next_sample();
+		if (!sample) {
 			// TODO wait if necessary
 			continue;
 		}
 		// Generate oscillator IQ
-		double time = sample.getIndex() / sample.getSampleRate();
-		complex bfo_sample = cos(frequency * time) + i * sin(frequency * time);
+		double sig_time = sample->getIndex() / sample->getSampleRate();
+		complex<float> bfo_sample(cos(OSC_freq * sig_time), sin(
+		                              OSC_freq * sig_time));
 		// Multiply sample
-		complex sig_sample = sample.get_data();
+		complex<float> sig_sample = sample->getData();
 		sig_sample *= bfo_sample;
-		sample.setData(sig_sample);
+		sample->setData(sig_sample);
 		// Queue sample
 		output_queue_mutex.lock();
-		while(signal_queue.size() >= QUEUE_SIZE_MAX){
+		while (output_queue.size() >= QUEUE_SIZE_MAX) {
 			output_queue_mutex.unlock();
 			// TODO force wait using posix wait
 			output_queue_mutex.lock();
--- cw_detect/BFO.hpp
+++ cw_detect/BFO.hpp
@@ -14,62 +14,62 @@ using namespace std;
  *
  * @author NATHAN HUI <nthui@gmail.com>
  */
-class BFO{
-	private:
-		/**
-		 * Sampling rate of the incoming stream in samples per second
-		 */
-		int sample_rate;
-		/**
-		 * Frequency of the beat frequency oscillator.
-		 */
-		int OSC_freq;
-		/**
-		 * Function pointer to the function that returns the next sample
-		 */
-		CRFSample (*next_sample)();
-		/**
-		 * Thread body
-		 */
-		void run();
-		/**
-		 * The thread object associated with this class
-		 */
-		thread class_thread;
-		/**
-		 * The output queue
-		 */
-		queue<CRFSample> output_queue;
-		/**
-		 * Mutex for locking the output queue
-		 */
-		mutex output_queue_mutex;
-		/**
-		 * Run state variable
-		 */
-		bool run_state;
-	public:
-		/**
-		 * Creates a BFO class and initializes the workflow associated with
-		 * this class.
-		 *
-		 * @param sample_rate Sampling rate of the incoming queue
-		 * @param frequency	Frequency for the beat frequency oscillator
-		 * @param out_queue	Function that returns in sequence the signal to
-		 * 					modify.
-		 */
-		BFO(int sample_rate, int frequency, CRFSample (*out_queue)());
-		/**
-		 * Destrpys this BFO class and deactivates the workflow associated with
-		 * this class.
-		 */
-		~BFO();
-		/**
-		 * Returns the next processed signal sample in the queue.  If none exist,
-		 * return null.
-		 *
-		 * @return the next processed sample, or NULL if none exist
-		 */
-		CRFSample getNextSample();
+class BFO {
+private:
+	/**
+	 * Sampling rate of the incoming stream in samples per second
+	 */
+	int sample_rate;
+	/**
+	 * Frequency of the beat frequency oscillator.
+	 */
+	int OSC_freq;
+	/**
+	 * Function pointer to the function that returns the next sample
+	 */
+	CRFSample* (*next_sample)();
+	/**
+	 * Thread body
+	 */
+	void run();
+	/**
+	 * The thread object associated with this class
+	 */
+	thread class_thread;
+	/**
+	 * The output queue
+	 */
+	queue<CRFSample*> output_queue;
+	/**
+	 * Mutex for locking the output queue
+	 */
+	mutex output_queue_mutex;
+	/**
+	 * Run state variable
+	 */
+	bool run_state;
+public:
+	/**
+	 * Creates a BFO class and initializes the workflow associated with
+	 * this class.
+	 *
+	 * @param sample_rate Sampling rate of the incoming queue
+	 * @param frequency	Frequency for the beat frequency oscillator
+	 * @param out_queue	Function that returns in sequence the signal to
+	 * 					modify.
+	 */
+	BFO(int sample_rate, int frequency, CRFSample * (*out_queue)());
+	/**
+	 * Destrpys this BFO class and deactivates the workflow associated with
+	 * this class.
+	 */
+	~BFO();
+	/**
+	 * Returns the next processed signal sample in the queue.  If none exist,
+	 * return null.
+	 *
+	 * @return the next processed sample, or NULL if none exist
+	 */
+	CRFSample* getNextSample();
 };
 #endif	// __BFO_H_
