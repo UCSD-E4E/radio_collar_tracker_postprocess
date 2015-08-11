8d0a03bf818ab27aaaf76b0d6b4bdd58b22accfb
ntlhui <ntlhui@gmail.com>
2015-08-11 15:01:59 -0700

    Started header

---
+++ cw_detect/FileWriter.hpp
@@ -0,0 +1,11 @@
+#ifndef __FILE_WRITER_H_
+#define __FILE_WRITER_H_
+
+#include <queue>
+#include "Sample.hpp"
+#include <string>
+#include <thread>
+#include <mutex>
+#include <fstram>
+
+#endif //__FILE_WRITER_H_
