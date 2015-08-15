e8f7c55a40d7440bd14710bf2dbe89ce2416e3a7
ntlhui <ntlhui@gmail.com>
2015-08-15 14:35:51 -0700

    Added print statements

--- cw_detect/Starter.cpp
+++ cw_detect/Starter.cpp
@@ -7,6 +7,7 @@
 #include "FileLoader.hpp"
 #include "FileWriter.hpp"
 #include "Sample.hpp"
+#include <iostream>
 
 /**
  * This is the main routine for this program.  This function initializes and
@@ -16,14 +17,18 @@
  * @param argv	Array of C strings representing arguments
  */
 int main(int argc, char** argv){
+	cout << "Starter: Loading classes" << endl;
 	RFFileLoader* file_loader = new RFFileLoader(2048000);
 	FileWriter* file_writer = new FileWriter(2048000, file_loader, 1);
 	file_writer->setMetaSuffix(".test");
 	file_writer->setDataSuffix(".test");
 	file_writer->setDataDir("./test/");
 	file_writer->start();
+	cout << "Starter: Adding file" << endl;
 	file_loader->addFile("test.raw");
+	cout << "Starter: Signaling Termination" << endl;
 	file_loader->sendTerminating();
+	cout << "Starter: Deleting classes" << endl;
 	delete file_writer;
 	delete file_loader;
 	return 0;
