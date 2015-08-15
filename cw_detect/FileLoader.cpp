220c23520c1e26f06517e0b1a46fb9ad8dba2221
ntlhui <ntlhui@gmail.com>
2015-08-15 16:35:37 -0700

    Added terminating signal handling, added debugging statements

--- cw_detect/FileLoader.cpp
+++ cw_detect/FileLoader.cpp
@@ -6,6 +6,7 @@
 #include <cstdint>
 #include <sys/stat.h>
 #include <stdexcept>
+#include <iostream>
 using namespace std;
 
 #ifndef QUEUE_SIZE_MAX
@@ -14,6 +15,7 @@ using namespace std;
 
 void RFFileLoader::run() {
 	// Make variables
+	cout << "FileLoader: Starting" << endl;
 	string file_to_load;
 	ifstream file_stream;
 	uint8_t buffer[2];	// read buffer
@@ -21,48 +23,54 @@ void RFFileLoader::run() {
 
 	// thread body
 	while (run_state) {
-		// Lock queue
-		file_queue_mutex.lock();
 		// check if empty
 		if (!file_queue.empty()) {
+			file_queue_mutex.lock();
 			// Get file
 			file_to_load = file_queue.front();
 			file_queue.pop();
 			// Unlock
 			file_queue_mutex.unlock();
 
+			// Check for terminating signal
+			if(file_to_load == "*"){
+				// Got terminating signal
+				cout << "FileLoader: Processed terminating signal" << endl;
+				break;
+			}
+			cout << "FileLoader: Processing file " << file_to_load << endl;
+
 			// Make stream
 			file_stream.open(file_to_load, ios::in | ios::binary);
 			// If fail
 			if (file_stream.fail()) {
-				cerr << "Failed to open file!" << endl;
+				cerr << "FileLoader: ERROR: Failed to open file!" << endl;
 			}
 
 			// while read 2 into the buffer
 			while (file_stream.read((char*)buffer, 2)) {
 				// Load into queue if space
-				signal_queue_mutex.lock();
 				while (signal_queue.size() >= QUEUE_SIZE_MAX) {
-					signal_queue_mutex.unlock();
 					// wait
+					cout << "FileLoader: Output Queue Full!" << endl;
 					// TODO force using posix wait
-					signal_queue_mutex.lock();
 				}
 
+				signal_queue_mutex.lock();
 				CRFSample* new_sample = new CRFSample(index, sample_rate,
 						buffer[0] / 128.0 - 0.5, buffer[1] / 128.0 - 0.5);
 				signal_queue.push(new_sample);
 				signal_queue_mutex.unlock();
 				index++;
-
 			}
+			cout << "FileLoader: Loaded all samples.  Current count: " << index
+				<< endl;
 			// close stream
 			file_stream.close();
 		} else {
 			// queue empty
 
-			// unlock queue
-			file_queue_mutex.unlock();
+			cout << "FileLoader: No files to process." << endl;
 
 			// wait
 			// TODO use conditional variable.  For now, just bash
@@ -70,11 +78,13 @@ void RFFileLoader::run() {
 	}
 
 	// Send terminating signal
+	cout << "FileLoader: Sending Termination Sample" << endl;
 	signal_queue_mutex.lock();
 	CRFSample* new_sample = new CRFSample(index, sample_rate, 0, 0);
 	new_sample->setTerminating();
 	signal_queue.push(new_sample);
 	signal_queue_mutex.unlock();
+	cout << "FileLoader: Ending thread" << endl;
 }
 
 RFFileLoader::RFFileLoader(int sample_rate) : sample_rate(sample_rate),
@@ -102,14 +112,16 @@ RFFileLoader::~RFFileLoader() {
 }
 
 void RFFileLoader::sendTerminating(){
-	run_state = false;
+	file_queue_mutex.lock();
+	file_queue.push("*");
+	file_queue_mutex.unlock();
 }
 
 CRFSample* RFFileLoader::getNextSample() {
-	signal_queue_mutex.lock();
 	if (signal_queue.empty()) {
 		return NULL;
 	}
+	signal_queue_mutex.lock();
 	CRFSample* retval = signal_queue.front();
 	signal_queue.pop();
 	signal_queue_mutex.unlock();
