6fe2a4e942993c8b5557087aa4844ad85c3f3d2e
ntlhui <ntlhui@gmail.com>
2015-09-10 08:01:20 -0700

    Added configuration file read

--- CLI_GUI/cas2.sh
+++ CLI_GUI/cas2.sh
@@ -5,6 +5,7 @@ RAW_DATA_COMPILER='/home/ntlhui/workspace/radio_collar_tracker/raw_gps_analysis/
 DISPLAY_DATA='/home/ntlhui/workspace/radio_collar_tracker/collarDisplay/display_data.py'
 META_FILE_READER='/home/ntlhui/workspace/radio_collar_tracker/meta_file_reader/read_meta_file.py'
 FREQUENCY_CALCULATOR='/home/ntlhui/workspace/radio_collar_tracker/ppm_adjust/get_beat_frequency.py'
+CONFIG_DIR='/home/ntlhui/workspace/radio_collar_tracker/config/'
 # User supplied configuration
 OPTIND=1
 if [[ $# -eq 0 ]]
@@ -44,14 +45,12 @@ done
 
 # Generated variables
 num_raw_files=`ls ${data_dir} | grep 'RAW_DATA_' | wc -l`
-# TODO fix this to be modular
-num_collars=1
+num_collars=`cat ${CONFIG_DIR}/COL | wc -l`
 raw_file=`printf "%s/RUN_%06d.raw" ${data_dir} ${run}`
 collar_file_prefix=`printf "%s/RUN_%06d_" ${data_dir} ${run}`
 meta_file=`printf "%s/META_%06d" ${data_dir} ${run}`
 sdr_center_freq=`${META_FILE_READER} -i ${meta_file} -t center_freq`
-# TODO fix this to be modular!
-sdr_ppm=-64
+sdr_ppm=`${META_FILE_READER} -i ${CONFIG_DIR}/SDR.cfg -t sdr_ppm`
 
 # Concatenate raw files together
 if [[ -e $raw_file ]]
@@ -67,7 +66,7 @@ done
 for i in `seq 1 ${num_collars}`
 do
 	# Get collar beat frequency
-	frequency=168059000
+	frequency=`${META_FILE_READER} -i ${CONFIG_DIR}/COL -t ${i}`
 	beat_freq=`${FREQUENCY_CALCULATOR} -f ${sdr_center_freq} -c ${frequency} -e ${sdr_ppm}`
 	# Execute pipeline
 	${GNU_RADIO_PIPELINE} -f ${beat_freq} -i ${raw_file} -o `printf "%s%06d.raw" ${collar_file_prefix} ${i}`
