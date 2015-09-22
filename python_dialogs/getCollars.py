339ade0f0c199e7be7ae69c034be8c55705e113d
E4E <e4e@E4Es-MacBook-Air.local>
2015-09-21 18:34:55 -0700

    Added python dialogs

---
+++ python_dialogs/getCollars.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import Tkinter as tk
+import tkSimpleDialog
+
+root = tk.Tk()
+root.withdraw()
+counter = 1
+hasOutput = False
+while True:
+	frequency = tkSimpleDialog.askinteger("RCT Post-Processing Pipeline", "Run Number:")
+	if frequency is not None:
+		print("%d: %d" % (counter, frequency))
+		counter += 1
+		hasOutput = True
+	else:
+		if not hasOutput:
+			exit(1)
+		else:
+			exit()
