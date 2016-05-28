2c66e65fe73e6031c515b85f5b60a6c540dd4983
ntlhui <ntlhui@gmail.com>
2016-05-28 18:02:49 -0500

    Revert "Added basic implementation"
    This reverts commit 58abe6741c72c507f60e744de320e72aa2ea6c05.

--- python_dialogs/getCollars.py
+++ python_dialogs/getCollars.py
@@ -9,15 +9,7 @@ hasOutput = False
 while True:
 	frequency = tkSimpleDialog.askinteger("RCT Post-Processing Pipeline", "Collar Frequency, press Cancel to finish:")
 	if frequency is not None:
-		print("%d: %d" % (counter, frequency - 1000))
-		counter += 1
-		print("%d: %d" % (counter, frequency - 500))
-		counter += 1
 		print("%d: %d" % (counter, frequency))
-		counter += 1
-		print("%d: %d" % (counter, frequency + 500))
-		counter += 1
-		print("%d: %d" % (counter, frequency + 1000))
 		counter += 1
 		hasOutput = True
 	else:
