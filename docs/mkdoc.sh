1681ba6f692e27fd4f8fb1dd6a2b30aab9941c5b
ntlhui <ntlhui@gmail.com>
2016-05-30 06:58:53 -0500

    Added rct_manual

---
+++ docs/mkdoc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pdflatex -halt-on-error rct_manual.tex -output-directory .
+makeglossaries rct_manual
+pdflatex -halt-on-error rct_manual.tex -output-directory .
+makeglossaries rct_manual
+pdflatex -halt-on-error rct_manual.tex -output-directory .
+rm *.aux *.glg *.glo *.gls *.ist *.log *.toc *.out
