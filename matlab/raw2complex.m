348d3e54a8122d031db88f070ce24643c8fe2fb8
Nathan Hui <ntlhui@gmail.com>
2017-07-21 18:32:42 -0700

    Added code for old SDR data

--- matlab/raw2complex.m
+++ matlab/raw2complex.m
@@ -3,9 +3,13 @@ function [data] = raw2complex(filename)
 %values
 
 fileID = fopen(filename);
+% bin = fread(fileID, 'uint8');
 bin = fread(fileID, 'int16');
 
 length = size(bin, 1);
+
+% real = bin(1:2:length)/128.0 - 1;
+% imag = bin(2:2:length)/128.0 - 1;
 
 real = bin(1:2:length)/4096.0;
 imag = bin(2:2:length)/4096.0;
