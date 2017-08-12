b4fb0bc9a9f24a11ae37c8b939d3da95859831f5
Anthony Koutroulis <aakoutro@ucsc.edu>
2017-08-11 17:30:31 -0700

    Fix: Push correct version of WIP files

--- python_scripts/fft_detect.py
+++ python_scripts/fft_detect.py
@@ -4,89 +4,249 @@ Created on Thu Jul 27 15:10:11 2017
 
 @author: anthony
 """
+#%%
 import rct
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set() # styling
 import scipy.signal as sg
+import peakutils as pu
 
+#%% Run Variables
+# original tests performed on run10 from canyon series
+#run = '16';
+#series = 'desert'
+run = '47';
+series = 'canyon2'
+path = 'C:\\Users\\anthony\\Desktop\\e4e\\'+series+'\\' # Inside here should be a RUN_000## folder
 
-run = '10' #10
-series = 'canyon' #canyon
-path = 'C:\\Users\\anthony\\Desktop\\'+series+'\\'
-
-Fs          = 2000000
-Fx          = 172950830
-Fc          = 172500000
+# TODO: Use META_DATA file for literals and run variables
+Fsx         = 2000000.0       # sampling frequency of input data
+Fx          = 172950830.0
+Fc          = 172500000.0
 FFT_LENGTH  = 4096
-Fs_fdata    = Fs/FFT_LENGTH
+Fsf         = Fsx/FFT_LENGTH # sampling frquency of fft
 
 pW          = 0.02 # pulse width [seconds]
-pT          = 1.48 # pulse period [seconds]
+pT          = 1.6 # pulse period [seconds]
+rct.Pulse.setPulsePeriod(pT)
 
-xbin        = int(np.floor((Fx - Fc) / Fs * FFT_LENGTH))
-raw_files   = rct.getfiles(path,run)
-
-data    = []
-fdata   = []
-fdataL  = []
-fdataR  = []
-extra   = []
-
-w = sg.get_window('flattop',FFT_LENGTH)
-
-# OPTIONAL: Halfdata for speedup
-#raw_files = raw_files[:int(np.ceil(len(raw_files)/2))]
-
-for rf in raw_files :
-    data = extra;extra=[]
-    data.extend(rct.raw2complex(rf))
-    for d in range(0,len(data),FFT_LENGTH):
-        fft_in = (data[d:d+FFT_LENGTH] * w) / FFT_LENGTH
-        fft_out = np.fft.fft(fft_in);fft_in=[]
-        fdata.append(fft_out[xbin])
-        fdataL.append(fft_out[xbin-1])
-        fdataR.append(fft_out[xbin+1]);fft_out=[]
-    extra = data[d+FFT_LENGTH+1:]  
-
-fdata = np.array(fdata)
-
-t = np.linspace(1/Fs, len(data)/Fs, len(data))
-#ft = np.linspace(1/Fs,len(fdata)/Fs,len(fdata))
-ft = np.linspace(1/Fs_fdata,len(fdata)/Fs_fdata,len(fdata))
-
-plt.figure(1)
-plt.subplot(1,3,1)
-plt.plot(ft,abs(np.array(fdataL)))
-plt.title('Left of Target Bin')
-plt.ylabel("Amplitude")
-
-plt.subplot(1,3,2)
-plt.plot(ft,abs(fdata))
-plt.title('Target Bin')
-plt.xlabel("Time [s]")
-
-plt.subplot(1,3,3)
-plt.plot(ft,abs(np.array(fdataR)))
-plt.title('Right of Target Bin')
+xbin               = int( np.round( (Fx - Fc) / Fsx * FFT_LENGTH ) )
+raw_files, meta   = rct.getfiles(path,run)
+raw_files   = raw_files[-12:]
 
 
+#%% FFT
+# return the power spectrum (time domain)
+Px = rct.fftFromFiles2(raw_files,FFT_LENGTH,xbin,Fsx,w='boxcar')
+
+# frequency time axis (length of fft)
+ft = np.linspace(1/Fsf,len(Px)/Fsf,len(Px))
+
+# find the peak sample (most likely to be a near pulse)
+midx = np.argmax(Px)
 
 
-Px = abs(fdata**2)
-#pn = np.mean(Px[150:450])
-#ps = np.sqrt(Px[:int(round(len(Px)/2))].max()) - pn
-#SNR = 10*np.log10(ps/pn)
-#print(SNR)
+truePeriod = rct.findTruePeriod(Px,pW,pT,Fsf)
+trueWidth = rct.findTrueWidth(Px,pW,pT,Fsf)
+print("TW adjust, pass 1:")
+print("T,W: "+str(pT)+'-->'+str(truePeriod)+','+str(pW)+'-->'+str(trueWidth))
+
+truePeriod = rct.findTruePeriod(Px,trueWidth,truePeriod,Fsf)
+trueWidth = rct.findTrueWidth(Px,trueWidth,truePeriod,Fsf)
+print("TW adjust, pass 2:")
+print("T,W: "+str(truePeriod)+','+str(trueWidth))
+
+truePeriod = rct.findTruePeriod(Px,trueWidth,truePeriod,Fsf)
+trueWidth = rct.findTrueWidth(Px,trueWidth,truePeriod,Fsf)
+print("TW adjust, pass 3:")
+print("T,W: "+str(truePeriod)+','+str(trueWidth))
+
+rct.Pulse.setPulsePeriod(truePeriod)
+rct.Pulse.setPulseWidth(trueWidth)
+
+
+#sample1 = Px[:int(pT*Fsf)]
+#sample2 = Px[int(1*pT*Fsf):int(2*pT*Fsf)]
+#sample3 = Px[int(2*pT*Fsf):int(3*pT*Fsf)]
+#sample4 = Px[int(3*pT*Fsf):int(4*pT*Fsf)]
+#sample = np.concatenate((sample1,sample2,sample3,sample4))
+#t1 = np.linspace(1/Fsf,len(sample1)/Fsf,len(sample1))
+#t2 = np.linspace(t1[-1] + len(sample2)/Fsf,len(sample2)/Fsf,len(sample2))
+#t3 = np.linspace(t2[-1] + len(sample3)/Fsf,len(sample3)/Fsf,len(sample3))
+#t4 = np.linspace(t3[-1] + len(sample4)/Fsf,len(sample4)/Fsf,len(sample4))
+#t = np.concatenate((t1,t2,t3,t4))
 #
+#
+#
+#plt.figure(1)
+#plt.subplot(1,4,1)
+#plt.plot(t1,sample1)
+#plt.subplot(1,4,2)
+#plt.plot(t2,sample2)
+#plt.subplot(1,4,3)
+#plt.plot(t3,sample3)
+#plt.subplot(1,4,4)
+#plt.plot(t4,sample4)
+ 
+#%% Plotting Fre quency Windows
+#plt.figure(1) # plot frequency bin and adjacent bins
+#plt.subplot(1,3,1)
+#plt.plot(ft,abs(np.array(fdataL)))
+#plt.title('Left of Target Bin')
+#plt.ylabel("Amplitude")
+#
+#plt.subplot(1,3,2)
+#plt.plot(ft,abs(fdata))
+#plt.title('Target Bin')
+#plt.xlabel("Time [s]")
+#
+#plt.subplot(1,3,3)
+#plt.plot(ft,abs(np.array(fdataR)))
+#plt.title('Right of Target Bin')
+
+#%% Sample rct.Pulse Use
 #sample = Px[:int(np.ceil(1.6*Fs_fdata))]
 #potpulse = rct.Pulse(sample)
 
 
+#%% Plot Pulses and Power Spectrum
+plt.figure(1)
+plt.plot(ft,Px,label='Power Spectrum with Pulse Prediction')
+plt.title('Detected Pulses in Power Spectrum')
+
+# TODO: find best two
+pW = trueWidth
+pT = truePeriod
+#rct.Pulse.setPulsePeriod((np.median(pulses[1].peaks) - np.median(pulses[0].peaks))/Fsf)
+rct.Pulse.setPulsePeriod(truePeriod)
+pulses = rct.Pulse.findPulses(Px)
+    
+
+rct.plotPulses(pulses,ft,Px)
+#rct.plotPeriods(ft,Fsf,rct.Pulse.PULSE_PERIOD,np.argmax(Px))
+
+rct.plotPeriods(ft,Fsf,truePeriod,np.argmax(Px),'y')
+
+
+import matplotlib.lines as mlines
+
+blu_lin = mlines.Line2D([],[],c='b')
+mag_lin = mlines.Line2D([],[],c='m',marker='X')
+red_dot = plt.scatter([],[],c='r')
+org_dot = plt.scatter([],[],c='orange')
+plt.legend([blu_lin,mag_lin,red_dot,org_dot],
+           ['Power Spectrum','Expected Pulse','Detected Pulse','Possible Pulse'])
+plt.ylabel("Power")
+#plt.xlabel("Time [s]")
+#avgSNR = np.sum([p.snr for p in pulses])/ rct.Pulse.pulseCount
+#plt.text(0.1,0.1,'Average SNR = '+str(round(avgSNR,2))+' dB'
+#         ,transform=plt.gca().transAxes,
+#         bbox=dict(facecolor='white',edgecolor='black'))
+#plt.legend()
+
+#%% smooth moves
+#sPx = sg.medfilt(Px)
+sPx9 = sg.medfilt(Px,9)
+pW = rct.findTrueWidth(sPx9,pW,pT,Fsf)
+#sPx49 = sg.medfilt(Px,49)
+#sPx999 = sg.medfilt(Px,999)
+#
+#plt.figure(2)
+#plt.subplot(3,2,1)
+#plt.plot(ft,Px)
+#plt.plot(ft,sPx)
+#plt.title('Median Filter, N=3')
+#plt.subplot(3,2,2)
+#plt.plot(ft,Px)
+#plt.plot(ft,sPx9)
+#plt.title('Median Filter, N=9')
+#plt.subplot(3,2,3)
+#plt.plot(ft,Px)
+#plt.plot(ft,sPx49)
+#plt.title('Median Filter, N=49')
+#plt.subplot(3,2,4)
+#plt.plot(ft,Px)
+#plt.plot(ft,sPx999)
+#plt.title('Median Filter, N=999')
+#plt.subplot(3,2,5)
+#plt.plot(ft,sPx)
+#plt.plot(ft,sg.medfilt(sPx,3))
+#plt.plot(ft,sg.medfilt(sPx,9))
+#plt.legend(['Pass1','Pass2, N=3', 'Pass2, N=9'])
+#plt.title('Median Filter, N=3 (2nd Pass)')
+#plt.subplot(3,2,6)
+#plt.plot(ft,sPx9)
+#plt.plot(ft,sg.medfilt(sPx9,3))
+#plt.plot(ft,sg.medfilt(sPx9,9))
+#plt.legend(['Pass1','Pass2, N=3', 'Pass2, N=9'])
+#plt.title('Median Filter, N=9 (2nd Pass)')
+###CONCLUSION: median filter with N = 9 is best
+
+#%% TEST Flattop test for making well defined peaks
+## CONCLUSION Flattop works. 49 good balance, more makes all peaks(noise) higher
+#plt.figure(3)
+#plt.plot(ft,sPx9)
+smoothPeriod = rct.findTruePeriod(sPx9,pW,truePeriod,Fsf)
+#rct.plotPeriods(ft,Fsf,smoothPeriod,np.argmax(sPx9))
+
+#flt9 = sg.convolve(sPx9,sg.flattop(9),mode='same')
+flt49 = sg.convolve(sPx9,sg.flattop(49),mode='same')
+#flt99 = sg.convolve(sPx9,sg.flattop(99),mode='same')
 
 plt.figure(2)
-plt.plot(ft,Px,label='Power Spectrum')
-pulses = rct.Pulse.findPulses(Px)
-rct.plotPulses(pulses,ft,Px)
-rct.plotPeriods(ft,Fs_fdata,1.48)
-#plt.legend()
+plt.plot(ft,flt49)
+flatPeriod = rct.findTruePeriod(flt49,pW,smoothPeriod,Fsf)
+rct.plotPeriods(ft,Fsf,flatPeriod,np.argmax(flt49))
+adj = 4 # magic number?
+peaks = pu.indexes(flt49,thres=0,min_dist=flatPeriod*Fsf - adj)
+plt.scatter(ft[peaks],flt49[peaks],color='r')
+
+#%% ricker wavelet for pulse width detection
+
+###TEST 1 -- assumes squareish --- RESULT: pW = 0.006144 (way too thin)
+#same = True; tops = []; top_val = test[np.argmax(test)];
+#while(same):
+#    top = np.argmax(test)
+#    tops.append(top)
+#    test[top] *= -1
+#    if not np.allclose(abs(test[tops]),[top_val]):
+#        same = False
+#        test[tops] *= -1
+#        tops = tops[:-1]
+
+# 9,1 hat on sPx9 is best
+#hat = sg.convolve(Px,sg.ricker(9,1), mode='same')
+smoothhat = sg.convolve(sPx9,sg.ricker(9,1),mode='same')
+#flathat = sg.convolve(flt49,sg.ricker(9,1),mode='same')
+#plt.figure(5)
+#plt.subplot(1,3,1)
+#plt.plot(ft,Px)
+#plt.plot(ft,hat)
+#plt.subplot(1,3,2)
+#plt.plot(ft,sPx9)
+#plt.plot(ft,smoothhat)
+#plt.subplot(1,3,3)
+#plt.plot(ft,flt49)
+#plt.plot(ft,flathat)
+
+#%% Welch exp
+#plt.figure(2)
+#plt.subplot(2,1,1)
+##plt.semilogy(ft,abs(fdata)/FFT_LENGTH)
+#plt.title("Nathan's Method")
+#plt.subplot(2,1,2)
+#plt.semilogy(ft,fdata2)
+#plt.title("SciPy's Welch Method")
+
+midx = np.argmax(flt49)
+
+per = rct.predictPeriods(ft, Fsx, pT, midx)
+pul = rct.predictPulses(ft, Fsx, pT, midx)
+plt.figure(6)
+plt.plot(ft,flt49)
+rct.plotPeriods2(ft,per)
+rct.plotPulses2(ft,pul)
+adj = .8 # magic number?
+peaks = pu.indexes(flt49,thres=0,min_dist=flatPeriod*Fsf*adj)
+plt.scatter(ft[peaks],flt49[peaks],color='r')
--- python_scripts/rct.py
+++ python_scripts/rct.py
@@ -5,32 +5,27 @@ Created on Thu Jul 27 15:04:21 2017
 @author: anthony
 """
 import numpy as np
+import scipy.signal as sg
+import matplotlib.pyplot as plt
 
 class Pulse:
-    PULSE_WIDTH     = 0.02  # seconds
-    PULSE_PERIOD    = 1.48   # seconds
-    pulseCount      = 0     # number of pulses accounted for
+    PULSE_WIDTH     = 0.02   # seconds
+    PULSE_PERIOD    = 1.6    # seconds
+    pulseCount      = 0      # number of pulses accounted for
     
     fs = 2000000 / 4096 # TODO: make this dynamic or something
-    __pWidxs = int(np.floor(PULSE_WIDTH*fs))
-    __pTidxs = int(np.floor(PULSE_PERIOD*fs))
+    pWidxs = int(np.floor(PULSE_WIDTH*fs))    # pulse width array indices
+    pTidxs = int(np.floor(PULSE_PERIOD*fs))   # pulse period array indices
     
     
     def __init__(self,sample,offset=0):
         # TODO: add error checking here
         Pulse.pulseCount += 1
-#        Pulse.__pWidxs = int(np.floor(Pulse.PULSE_WIDTH*self.fs))
-#        Pulse.__pTidxs = int(np.floor(Pulse.PULSE_PERIOD*self.fs))
-#        self.peaks = peaks; self.noise = noise; self.snr = float()
         self.data = sample
         self.definePulse(); self.defineNoise(); self.defineSNR();
         self.peaks = self.peaks + offset
         self.noise = self.noise + offset
-        
-
-            
-        # TODO: add class functions to constructor for simpler use.
-    
+                
         # TODO: make this work
     def __repr__(self):
         return str(self.peaks)
@@ -41,32 +36,49 @@ class Pulse:
     
     def definePulse(self):
         self.peaks = []; data_copy = np.copy(self.data)
-        for i in range(0,self.__pWidxs):
+        for i in range(0,self.pWidxs):
             m_idx = np.argmax(data_copy)
             self.peaks.append(m_idx)
             data_copy[m_idx] = 0
         self.peaks = np.sort(self.peaks)
             
-    def defineNoise(self): # TODO does this need to force noise = [] ?
+    def defineNoise(self):
         if len(self.peaks) is 0 : self.definePulse()
         l,r = np.arange(0,self.peaks[0]),np.arange(self.peaks[-1]+1,len(self.data))
         self.noise = np.concatenate([l,r])
         
-        
     def defineSNR(self):
+        # Calculates the SNR of the pulse as avg signal power - avg noise power
         if len(self.noise) is 0 : self.defineNoise()
         pow_n = np.mean(self.data[self.noise])
         pow_s = np.mean(self.data[self.peaks]) - pow_n
         self.snr = 10*np.log10(pow_s) - 10*np.log10(pow_n)
         
+    def setPulseWidth(W):
+        # Updates the pulse width by taking W in seconds
+        Pulse.PULSE_WIDTH = W
+        Pulse.pWidxs = int(np.floor(Pulse.PULSE_WIDTH*Pulse.fs))
+        
+    def setPulsePeriod(T):
+        # Updates the pulse period by taking T in seconds
+        Pulse.PULSE_PERIOD = T
+        Pulse.pTidxs = int(np.floor(Pulse.PULSE_PERIOD*Pulse.fs))
+    
+    def setSamplingFrequency(fs):
+        # Updates the sampling frequency of the data
+        Pulse.fs = fs
+        Pulse.pWidxs = int(np.floor(Pulse.PULSE_WIDTH*Pulse.fs))
+        Pulse.pTidxs = int(np.floor(Pulse.PULSE_PERIOD*Pulse.fs))      
             
     def isWellDefined(self):
+        # Returns TRUE if all peaks are arranged sequentially
         consecutive_peaks = np.arange(self.peaks[0],self.peaks[-1]+1,1)
         return np.array_equal(self.peaks,consecutive_peaks)
     
     def findPulses(power_spectrum):
-        periods = range(0,len(power_spectrum),Pulse.__pTidxs)
-        pulses = [Pulse(power_spectrum[p:p+Pulse.__pTidxs],p) for p in periods]
+        # Returns a list of Pulse objects for each period in power spectrum
+        periods = range(0,len(power_spectrum),Pulse.pTidxs)
+        pulses = [Pulse(power_spectrum[p:p+Pulse.pTidxs],p) for p in periods]
         return pulses
             
 
@@ -77,45 +89,123 @@ def raw2complex(file):
     return iq
 
 def getfiles(run_folder,run_number):
+    # returns a list of individiual run files and a meta file
     import glob
     folder = run_folder + 'RUN_' + run_number.zfill(6) + '/'
     files  = 'RAW_DATA_' + run_number.zfill(6) + '*'
-    return glob.glob(folder + files)
+    meta   = 'META_' + run_number.zfill(6)
+    return glob.glob(folder + files), glob.glob(folder + meta)
 
+#def processMetaFile(metafile):
+#    start_time: 1501855851.174393
+#    center_freq: 172500000
+#    sampling_freq: 2000000
+#    gain: 76.00000
+#    np.loadtxt()
+#def defineNoiseFloor(pulses,data):
+#    noise_bin= []
+#    for p in pulses:
+#        if p.isWellDefined():
+#            noise_bin.append(np.median(data[p.noise]))
+#    return np.mean(noise_bin)
+#            
+#
+#def isPulse(pos,pulses):
+#    # Checks to see if there is a pulse at the provided index
 #    
-#def definePulse(sample,width,fs):
-#    maxima = []; sample = np.copy(sample);
-#    for i in range(0,int(np.floor(width*fs))) :
-#        m_idx = np.argmax(sample)
-#        maxima.append(m_idx)
-#        sample[m_idx] = 0
-#    return maxima
 #
-#def isWellDefined(pulse):
-#    pulse = np.sort(pulse)
-#    return np.array_equal(pulse,np.arange(pulse[0],pulse[-1]+1,1))
-#
-#def findPulses(data,period,width,fs):
-#    pulses = []; T = int(np.floor(period*fs))
-#    for i in range(0,len(data),T) :
-#        pulses.extend(np.array(definePulse(data[i:i+T],width,fs))+i)
-#    return pulses
+#def predictPulses(known_pulse_pos, pulse_period, t):
+#    for i in range(0,len(t),pulse_period): # plot to the right of known pulse
+#        
+#    
 
+#TODO fix precision on this
+def plotPeriods(t,fs,T,midx=0,c='y',n=1):
+    hp_idx = int(np.floor(T*fs/2))
+    err = T*fs/2 - hp_idx
+    #right
+    i = 0 + midx ; diff = 0
+    while ((i + hp_idx) < len(t)):
+        diff += err ; #print(diff)
+        if (diff >= 1) : i += 1 ; diff -=1
+        plt.axvline(t[i+hp_idx], color=c, linestyle='--')
+        plt.axvline(t[i], color='m',linewidth=int(0.02*fs),alpha=0.1)
+        plt.scatter(t[i],0,c='m',marker='X')
+        i += 2*hp_idx + int(np.ceil(4*err))
+#        print('Moving right: '+str(i)+'indexes')
+    #left
+    i = 0 + midx ; diff = 0
+    while ((i - hp_idx) >= 0):
+        diff += err ; #print(diff)
+        if (diff >= 1) :i -= 1 ; diff -=1
+        plt.axvline(t[i-hp_idx], color=c, linestyle='--')
+        plt.axvline(t[i], color='m',linewidth=int(0.02*fs),alpha=0.1)
+        plt.scatter(t[i],0,c='m',marker='X')
+        i -= 2*hp_idx - int(np.ceil(4*err)) - 1
+#        print('Moving left: '+str(i)+'indexes')
 
-def plotPeriods(t,fs,T):
-    import matplotlib.pyplot as plt
-    for i in range(0,len(t),int(np.floor(T*fs))):
-        plt.axvline(t[i], color = 'y', linestyle = '--')
+def plotPeriods2(t, periods, color='y', linestyle='--'):
+    for p in periods:
+        plt.axvline(t[p],c=color,linestyle=linestyle)
+
+def plotPulses2(t, pulses, color='m'):
+    for p in pulses:
+        plt.axvline(t[p], color=color,linewidth=int(0.02*t[0]),alpha=0.1)
+        plt.scatter(t[p],0,c=color,marker='X')
+
+def predictPeriods(t,fs,pT,midx=0):
+    mt = t[midx]
+    rT = []; lT = []
+    
+    for i in range(0,int(t[-1]/pT + 1),1):
+        rT.append(mt + pT*(i + 0.5))
+        
+    for i in range(0,int(t[midx]/pT + 1),1):
+        lT.append(mt - pT*(i + 0.5))
+        
+    lrT = np.concatenate((lT[::-1],rT))
+    
+    chunks = np.array_split(t,len(lrT))
+    periods = [np.abs(chunks[0]-lrT[0]).argmin()]
+    
+    for i in range(1,len(lrT),1):
+        periods.append((np.abs(chunks[i]-lrT[i]).argmin()) + i*len(chunks[i-1]))
+    
+    return periods
+
+def predictPulses(t,fs,pT,midx=0):
+    mt = t[midx]
+    rP = []; lP = []
+            
+    for i in range(0,int(t[-1]/pT + 1),1):
+        rP.append(mt + pT*i)
+    for i in range(0,int(t[midx]/pT + 1),1):
+        lP.append(mt - pT*i)
+
+    lrP = np.concatenate((lP[::-1],rP[1:]))
+
+    chunks = np.array_split(t,len(lrP))
+    pulses =  [np.abs(chunks[0]-lrP[0]).argmin()]
+    
+    for i in range(1,len(lrP),1):
+        pulses.append((np.abs(chunks[i]-lrP[i]).argmin()) + i*len(chunks[i-1]))
+
+    return pulses
 
 def plotPulses(pulses,t,xt):
     import matplotlib.pyplot as plt
     for pulse in pulses:
         if pulse.isWellDefined():
+            med = (t[int(np.median(pulse.peaks))],
+                         xt[int(np.max(pulse.peaks))])
             plt.scatter(t[pulse.peaks],xt[pulse.peaks],
                         c='red',marker='.',label='Detected Pulses')
-            plt.annotate(str(int(round(pulse.snr))),
-                        (t[int(np.median(pulse.peaks))],
-                         xt[int(np.max(pulse.peaks))]),
+            plt.annotate('SNR = '+str(int(round(pulse.snr))),
+                         xy = med,
+#                         xytext = (med[0]+0.1,med[1]+0.1),
+#                         arrowprops=dict(facecolor='black',shrink=0.05,
+#                                         width=1,headwidth=4,frac=0.2),
+#                         bbox=dict(facecolor='white',edgecolor='black'),
                          label='SNR')
         else:
             plt.scatter(t[pulse.peaks],xt[pulse.peaks],
@@ -124,3 +214,137 @@ def plotPulses(pulses,t,xt):
 #                         (t[int(np.median(pulse.peaks))],
 #                         xt[int(np.median(pulse.peaks))]))
 
+#
+#def movingAverageFilter(x,M):
+#    #y[i] = y[i - 1] + x[i + p] - x[i - q]
+#    #   p = (M - 1) / 2 ; M odd number of points
+#    #   q = p + 1
+#    acc = np.int64()
+#    for i in in x:
+#        acc = acc + x[i]
+
+def trimmedMean(pW, pT, fs, data):
+    trim = np.sort(data)
+    nPeaks = int(np.ceil(pW*fs))
+    nPulses = int(np.ceil(len(data)/(pT*fs)))
+    trim = trim[:nPeaks+nPulses]
+    return np.mean(trim)
+    
+def fftFromFiles(raw_files, fft_size, target_bin):
+    # Returns the fft of the data in the target frequency bin over time
+
+#    w = sg.get_window('blackmanharris',fft_size)
+#    once = True
+    fdata = []; extra = []; count = 0;
+    for rf in raw_files :
+        count += 1
+        print('Processing File '+str(count)+'/'+str(len(raw_files)))
+    
+        data = extra; extra = []
+        data.extend(raw2complex(rf))
+        
+        for d in range(0,len(data),fft_size):
+            
+#            print('Processing Data from '+str(d)+' to '+str(d+fft_size))
+#            print(str(len(data)-fft_size-d)+' data points remain')
+#            fft_in = np.array(data[d:d+fft_size]) / fft_size
+            fft_in = np.array(data[d:d+fft_size])
+            #test
+#            [p,c] = [np.argmax(fft_in),int(np.floor(len(fft_in)/2))]
+#            o = p-c; print(o)
+#            if o > 0 : w = np.concatenate((np.zeros(abs(o)),w[o:]))
+#            elif o < 0 : w = np.concatenate((np.zeros(abs(o)),w[:o]))
+#            if d is 0:
+#                plt.figure(10)
+#                plt.plot(fft_in)
+#                plt.plot(w)
+            
+            fft_out = np.fft.fft(fft_in) / fft_size; fft_in=[]
+            fdata.append(fft_out[target_bin])
+            if(d+fft_size > len(data)):
+                print('Copying leftover '+str(len(data[d+fft_size+1:]))+' data points')
+                extra = data[d+fft_size+1:] # THIS WAS MISINDENTED
+    return np.array(fdata)
+
+def fftFromFiles2(raw_files, fft_size, target_bin, fs, w='hann'):
+    count = 0        
+    fdata = []; extra = []
+    for rf in raw_files:
+        count += 1
+        print('Processing File '+str(count)+'/'+str(len(raw_files)))
+        data = extra; extra = []
+        data.extend(raw2complex(rf))
+        for d in range(0,len(data),fft_size):
+            fft_in = np.array(data[d:d+fft_size])
+            f,Pxx = sg.welch(fft_in,fs, nperseg=fft_size, window=w,
+                             return_onesided=False)
+            fdata.append(Pxx[target_bin])
+            if(d+fft_size > len(data)):
+                extra = data[d+fft_size+1:]
+    return np.array(fdata)
+
+
+def findTruePeriod(data, pW, pT, fs):
+    m_idx = np.argmax(data)
+    
+    pW_idx, pT_idx = [int(pW*fs*2),int(pT*fs)]
+    r_idx_a,r_idx_b = [m_idx+pW_idx,m_idx+pW_idx+int(pT_idx*1.5)]
+    l_idx_a,l_idx_b = [m_idx-pW_idx-int(pT_idx*1.5),m_idx-pW_idx]
+#    print(m_idx)
+    
+    if l_idx_a < 0 :
+        m_idx_l = m_idx
+        m_idx = 0
+        m_idx = np.argmax(data[r_idx_a:r_idx_b]) + r_idx_a
+        
+        r_idx_a,r_idx_b = [0,0]
+        r_idx_a,r_idx_b = [m_idx+pW_idx, m_idx+pW_idx+int(pT_idx*1.5)]
+#        print('Right is from: '+str(r_idx_a)+' to '+str(r_idx_b))
+        m_idx_r = np.argmax(data[r_idx_a:r_idx_b]) + r_idx_a
+
+#        print('Moving right, L,M,R = '+str(m_idx_l)+','+str(m_idx)+','+str(m_idx_r))
+
+    elif r_idx_b > len(data) :
+        m_idx_r = m_idx
+        m_idx = np.argmax(data[l_idx_a:l_idx_b]) + l_idx_a
+        
+        l_idx_a,l_idx_b = [m_idx-pW_idx-int(pT_idx*1.5),m_idx-pW_idx]
+        m_idx_l = np.argmax(data[l_idx_a:l_idx_b]) + l_idx_a
+
+#        print('Moving left, L,M,R = '+str(m_idx_l)+','+str(m_idx)+','+str(m_idx_r))
+
+    else:
+        m_idx_r = np.argmax(data[r_idx_a:r_idx_b]) + r_idx_a
+        m_idx_l = np.argmax(data[l_idx_a:l_idx_b]) + l_idx_a
+#        print('No need to move, L,M,R = '+str(m_idx_l)+','+str(m_idx)+','+str(m_idx_r))
+        
+        
+#    print('LeftT: '+str((m_idx_r-m_idx)/fs)+', RightT: '+str((m_idx-m_idx_l)/fs))
+    
+    # For plot testing
+#    import matplotlib.pyplot as plt
+#    t = np.linspace(1/fs,len(data)/fs,len(data))
+#    plt.scatter(t[m_idx],data[m_idx],c='c')
+#    plt.scatter(t[m_idx_l],data[m_idx_l],c='c')
+#    plt.scatter(t[m_idx_r],data[m_idx_r],c='c')
+#    
+    return np.mean([(m_idx_r-m_idx),(m_idx-m_idx_l)])/fs
+
+def findTrueWidth(data, pW, pT, fs):
+    m_idx = np.argmax(data)
+    hat = sg.convolve(data,sg.ricker(9,1),mode='same')
+    
+    lside,rside = [m_idx-int(pT*fs/2), m_idx+int(pT*fs/2)]
+    lmin = int(np.argmin(hat[lside:m_idx]) + lside)
+    rmin = int(np.argmin(hat[m_idx:rside]) + m_idx)
+    
+    return (rmin-lmin)/fs
+    
+    
+    
+    
+    
+    
+
+    
+    
