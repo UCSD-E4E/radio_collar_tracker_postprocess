a55634f02dd3fa288fa05240620dfcff3fea6bf0
Anthony Koutroulis <aakoutro@ucsc.edu>
2017-08-13 17:24:19 -0700

    Complete prediction and windowing demos

--- python_scripts/predict.py
+++ python_scripts/predict.py
@@ -5,7 +5,7 @@ Created on Fri Aug 11 14:52:55 2017
 @author: anthony
 """
 
-#%%
+#%% Dependencies
 import rct
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,31 +17,65 @@ import peakutils as pu
 # original tests performed on run10 from canyon series
 #run = '16';
 #series = 'desert'
-run = '47';
-series = 'canyon2'
-path = 'C:\\Users\\anthony\\Desktop\\e4e\\'+series+'\\' # Inside here should be a RUN_000## folder
-
+run = '19';
+series = 'desert'
+# Inside here should be a RUN_000## folder
+path = 'C:\\Users\\anthony\\Desktop\\e4e\\rct_runs\\'+series+'\\' 
 # TODO: Use META_DATA file for literals and run variables
 Fsx         = 2000000.0       # sampling frequency of input data
-Fx          = 172950830.0
+F_REAL      = 172950830.0   # REAL COLLAR
+F_FAKE      = 172600000.0   # FAKE COLLAR
+
+Fx          = F_REAL        # target collar frequency
 Fc          = 172500000.0
+#FFT_LENGTH  = int(4096 / 1.6)
 FFT_LENGTH  = 4096
 Fsf         = Fsx/FFT_LENGTH # sampling frquency of fft
 
 pW          = 0.02 # pulse width [seconds]
 pT          = 1.6 # pulse period [seconds]
 
-xbin        = int( np.round( (Fx - Fc) / Fsx * FFT_LENGTH ) )
-raw_files, meta   = rct.getfiles(path,run)
-raw_files   = raw_files[:1]
+xbin        = int( np.round( (Fx - Fc) / Fsx * FFT_LENGTH ) ) -1
+raw_files   = rct.getfiles(path,run)
+#raw_files   = raw_files[:6]
 
 
 #%% FFT
 # return the power spectrum (time domain)
-Px = rct.fftFromFiles2(raw_files,FFT_LENGTH,xbin,Fsx,w='boxcar')
+Pxx = rct.fftFromFiles2(raw_files,FFT_LENGTH,xbin,Fsx,w='boxcar')
+Px = sg.medfilt(Pxx,9)
 
 # frequency time axis (length of fft)
 ft = np.linspace(1/Fsf,len(Px)/Fsf,len(Px))
 
 # find the peak sample (most likely to be a near pulse)
 midx = np.argmax(Px)
+
+pT0 = pT
+pW0 = pW
+
+pulses0  = rct.predictPulses(ft, Fsf, pT0, midx)
+periods0 = rct.predictPeriods(ft, Fsf, pT0, midx)
+
+pT1 = rct.findTruePeriod(Px,pW,pT,Fsf)
+pW1 = rct.findTrueWidth(Px,pW,pT,Fsf)
+
+pulses1  = rct.predictPulses(ft, Fsf, pT1, midx)
+periods1 = rct.predictPeriods(ft, Fsf, pT1, midx)
+
+
+plt.figure(1)
+plt.subplot(1,2,1)
+plt.title('Predictions with Theoretical Pulse Specs')
+plt.plot(ft,Px)
+plt.scatter(ft[midx],Px[midx],c='y')
+rct.plotPeriods2(ft, periods0)
+rct.plotPulses2(ft, pulses0)
+
+plt.subplot(1,2,2)
+plt.title('Predictions with Autocalculated Pulse Specs')
+plt.plot(ft,Px)
+plt.scatter(ft[midx],Px[midx],c='y')
+rct.plotPeriods2(ft, periods1)
+rct.plotPulses2(ft, pulses1)
+
--- python_scripts/windows.py
+++ python_scripts/windows.py
@@ -16,14 +16,17 @@ import peakutils as pu
 # original tests performed on run10 from canyon series
 #run = '16';
 #series = 'desert'
-run = '47';
-series = 'canyon2'
+run = '31';
+series = 'anechoic2'
 path = 'C:\\Users\\anthony\\Desktop\\e4e\\'+series+'\\' # Inside here should be a RUN_000## folder
 
 # TODO: Use META_DATA file for literals and run variables
 Fsx         = 2000000.0       # sampling frequency of input data
-Fx          = 172950830.0
-Fc          = 172500000.0
+F_REAL      = 172950830.0   # REAL COLLAR
+F_FAKE      = 172600000.0   # FAKE COLLAR
+
+Fx          = F_REAL        # target collar frequency
+Fc          = 172500000.0   # Center frequency
 FFT_LENGTH  = 4096
 Fsf         = Fsx/FFT_LENGTH # sampling frquency of fft
 
@@ -31,6 +34,7 @@ pW          = 0.02 # pulse width [seconds]
 pT          = 1.6 # pulse period [seconds]
 
 xbin        = int( np.round( (Fx - Fc) / Fsx * FFT_LENGTH ) )
+refbin      = int( np.round( (F_FAKE - Fc) / Fsx * FFT_LENGTH ) )
 raw_files   = rct.getfiles(path,run)
 
 #%%
@@ -43,6 +47,8 @@ windows = ['boxcar', 'triang', 'blackman', 'hamming', 'hann', 'bartlett',
 #           'slepian (needs width)', 
 #           'chebwin (needs attenuation)
             ]
+
+# Look at one period of the time domain data
 x = rct.raw2complex(raw_files[0])
 x_slice = x[:int(pT*Fsx)]
 
@@ -53,29 +59,44 @@ for w in windows:
 [f_Pxx_1,f_Pxx_2] = np.split(np.array(f_Pxx),2)
 [w1, w2] = np.split(np.array(windows),2)
 
-plt.figure(420)
+plt.figure(1)
 
-plt.subplot(1,2,1)
+ax1 = plt.subplot(1,2,1)
 plt.title('Windowing effect on target frequency bin of the FFT 1/2')
-plt.axvline(f_Pxx[0][0][xbin], c='c', linestyle='--')    
+plt.axvline(f_Pxx[0][0][xbin], c='c', linestyle='--')
+plt.axvline(f_Pxx[0][0][refbin], c='m', linestyle='--')
 
 for f,Pxx in f_Pxx_1:
     plt.semilogy(f,Pxx)
 
-plt.legend(np.concatenate((['Target Bin'],w1)))
+plt.legend(np.concatenate((['Target Bin','Reference Bin'],w1)))
 plt.xlim(f_Pxx[0][0][xbin-3],f_Pxx[0][0][xbin+3])
-plt.ylim(7.5e-7,9.5e-7)
 
-plt.subplot(1,2,2)
+ax2 = plt.subplot(1,2,2, sharex=ax1, sharey=ax1)
 plt.title('Windowing effect on target frequency bin of the FFT 2/2')
-plt.axvline(f_Pxx[0][0][xbin], c='c', linestyle='--')    
+plt.axvline(f_Pxx[0][0][xbin], c='c', linestyle='--') 
+plt.axvline(f_Pxx[0][0][refbin], c='m', linestyle='--')   
 
 for f,Pxx in f_Pxx_2:
     plt.semilogy(f,Pxx)
 
-plt.legend(np.concatenate((['Target Bin'],w2)))    
+plt.legend(np.concatenate((['Target Bin','Reference Bin'],w2)))    
 plt.xlim(f_Pxx[0][0][xbin-3],f_Pxx[0][0][xbin+3])
-plt.ylim(7.5e-7,9.5e-7)
 
+#%% Compare window function to uniform in the fft reconstructed time domain
+w0 = np.ones(FFT_LENGTH)
+w1 = 'barthann'
 
-    
+Pxx0 = rct.fftFromFiles2([raw_files[0]],FFT_LENGTH,xbin,Fsx,w0)
+Pxx1 = rct.fftFromFiles2([raw_files[0]],FFT_LENGTH,xbin,Fsx,w1)
+
+ft = np.linspace(1/Fsf,len(Pxx0)/Fsf,len(Pxx0))
+
+plt.figure(2)
+ax3 = plt.subplot(1,2,1)
+plt.title('Frequency filter signal with a uniform window')
+plt.semilogy(ft, abs(Pxx0))
+
+ax4 = plt.subplot(1,2,2, sharex=ax3,sharey=ax3)
+plt.title('Frequency filter signal with a {0}'.format(w1))
+plt.semilogy(ft, abs(Pxx1))
