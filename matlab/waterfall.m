fa5fcc5c97e85c8fc6b20fdc5571928d7df136b1
Nathan Hui <ntlhui@gmail.com>
2017-08-17 22:52:52 -0500

    configured for dual sided spectrum

--- matlab/waterfall.m
+++ matlab/waterfall.m
@@ -8,15 +8,16 @@ function [] = waterfall(file)
 	num_ffts = floor(data_len / FFT_LEN);
 	fft_in_data = reshape(data(1:num_ffts * FFT_LEN), [FFT_LEN, num_ffts]);
 	fft_out_data = fft(fft_in_data);
+	fft_out_data = fftshift(fft_out_data);
 	P2 = abs(fft_out_data / FFT_LEN);
-	P2 = [P2(FFT_LEN/2+1:FFT_LEN,:); P2(1:FFT_LEN/2,:)];
+	% P2 = [P2(FFT_LEN/2+1:FFT_LEN,:); P2(1:FFT_LEN/2,:)];
 	waterfall_plot = 10*log10(abs(P2))';
 	figure('units','normalized','outerposition',[0 0 1 1]);
 	% imagesc(x, y, C)
-	y = [(0 + CENTER_FREQ) / 1e6, (1e6 + CENTER_FREQ) / 1e6];
+	y = [(CENTER_FREQ - 1e6) / 1e6, (CENTER_FREQ + 1e6) / 1e6];
 	% x = [1,FFT_LEN/2];
 	x = [0, data_len / 2000000];
-	C = waterfall_plot(:,FFT_LEN/2+1:FFT_LEN)';
+	C = waterfall_plot(:,1:FFT_LEN)';
 	clims = [max(std(waterfall_plot)) + mean(mean(waterfall_plot)), max(max(waterfall_plot))];
 	clims = [mean(mean(waterfall_plot)), max(max(waterfall_plot))];
 	imagesc(x, y, C, clims);
