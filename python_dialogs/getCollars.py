debe1307b70b7eba9de4b4de81ea785bc542ec61
Nathan Hui <ntlhui@gmail.com>
2017-08-18 14:02:38 -0500

    Removed extra freqs

--- python_dialogs/getCollars.py
+++ python_dialogs/getCollars.py
@@ -12,11 +12,11 @@ def getCollars():
 		frequency = tkSimpleDialog.askinteger("RCT Post-Processing Pipeline", "Collar Frequency, press Cancel to finish:")
 		root.update()
 		if frequency is not None:
-			retval.append(frequency - 1000)
-			retval.append(frequency - 500)
+			# retval.append(frequency - 1000)
+			# retval.append(frequency - 500)
 			retval.append(frequency)
-			retval.append(frequency + 500)
-			retval.append(frequency + 1000)
+			# retval.append(frequency + 500)
+			# retval.append(frequency + 1000)
 			hasOutput = True
 		else:
 			return retval
