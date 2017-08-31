ae4cba844d9fa9cb7d5b8b575c1d08110e8ea91c
Nathan Hui <ntlhui@gmail.com>
2017-08-30 21:15:17 -0500

    Removed GUI

--- utilities/colRename.py
+++ utilities/colRename.py
@@ -4,26 +4,32 @@ import getMappedCollars
 import os
 import glob
 import argparse
+import json
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument("run_dir")
 	args = parser.parse_args()
 	data_dir = args.run_dir
-	run_retval = getMappedCollars.getCollars(data_dir)
-	if run_retval is None:
-		exit()
-	run = run_retval['run']
-	col_db = getMappedCollars.collarDB()
+
 	collars = []
-	for ch in run_retval['tx']:
-		collars.append(int(col_db[ch]))
+	
+	runFile = open(os.path.join(data_dir, 'RUN'), 'r')
+	run = int(runFile.readline().split(':')[1].strip())
+
+	col_db = getMappedCollars.collarDB()
+
+	coljFile = open(os.path.join(data_dir, 'COLJ'), 'r')
+	freq_arr = json.loads(coljFile.readline())
+
+	for ch in freq_arr:
+		collars.append(int(col_db[unicode(ch)]))
 	numCollars = len(collars)
 
 	for i in xrange(numCollars):
 		print("Looking for collar %d" % (i + 1))
 		files = glob.glob('RUN_%06d_COL_%06d*' % (run, i + 1))
-		ch = int(run_retval['tx'][i])
+		ch = int(freq_arr[i])
 		print('CH %d is %d' % (ch, i + 1))
 		new_prefix = 'RUN_%06d_CH_%06d' % (run, ch)
 		for file in files:
