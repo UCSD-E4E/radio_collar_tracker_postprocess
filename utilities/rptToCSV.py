7a6c8083f4fbf82ad6f00cb1e46805f3e07b29cd
Nathan Hui <ntlhui@gmail.com>
2017-08-31 22:22:05 -0500

    Reformatted

--- utilities/rptToCSV.py
+++ utilities/rptToCSV.py
@@ -4,30 +4,50 @@ import os
 import argparse
 from dateutil.parser import parse
 from datetime import datetime
-parser = argparse.ArgumentParser('Generates the daily CSV report for RTT runs')
-parser.add_argument('-i', '--input_directory', required = True, help = 'Directory containing all the day\'s runs and results')
-parser.add_argument('-d', '--date', required = False, help = 'Date of runs')
-args = parser.parse_args()
 
-directory = args.input_directory
-if args.date is None:
-	date = datetime.today()
-else:
-	date = parse(args.date)
-datestr = date.strftime('%d-%m-%Y')
-reportname = date.strftime('%Y.%m.%d')
-csv = open('%s.csv' % reportname, 'w+')
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser('Generates the daily CSV report for RTT runs')
+	parser.add_argument('-i', '--input_directory', required = True, help = 'Directory containing all the day\'s runs and results')
+	parser.add_argument('-d', '--date', required = False, help = 'Date of runs')
+	args = parser.parse_args()
 
-csv.write('date,lat,lon,channel,time\n')
-for dirpath, dnames, fnames in os.walk(directory):
-	for f in fnames:
-		if f == 'Notes':
-			rpt = open(os.path.join(dirpath, f), 'r')
-			print(rpt)
-			while True:
+	directory = args.input_directory
+	if args.date is None:
+		date = datetime.today()
+	else:
+		date = parse(args.date)
+	datestr = date.strftime('%d-%m-%Y')
+	reportname = date.strftime('%Y.%m.%d')
+	csv = open('%s.csv' % reportname, 'w+')
+
+	csv.write('date,lat,lon,channel,time\n')
+	for dirpath, dnames, fnames in os.walk(directory):
+		for f in fnames:
+			if f == 'Notes':
+				rpt = open(os.path.join(dirpath, f), 'r')
+				print(rpt)
+				while True:
+					iguanaline = rpt.readline()
+					if iguanaline == '' or iguanaline is None:
+						break
+					iguana_num = int(iguanaline.split()[1])
+					print(iguana_num)
+					coordline = rpt.readline()
+					try:
+						lon = float(coordline.strip().split(',')[0].strip())
+						lat = float(coordline.strip().split(',')[1].strip())
+					except ValueError, e:
+						timeline = rpt.readline()
+						continue
+					errline = rpt.readline()
+					timeline = rpt.readline()
+					time = timeline.strip().split(' ')[0]
+					csv.write('%s,%.6f,%.6f,%d,%s\n' % (datestr,lat,lon,iguana_num,time))
+				rpt.close()
+			elif f.endswith('.rpt'):
+				rpt = open(os.path.join(dirpath, f), 'r')
+				print(rpt)
 				iguanaline = rpt.readline()
-				if iguanaline == '' or iguanaline is None:
-					break
 				iguana_num = int(iguanaline.split()[1])
 				print(iguana_num)
 				coordline = rpt.readline()
@@ -35,50 +55,11 @@ for dirpath, dnames, fnames in os.walk(directory):
 					lon = float(coordline.strip().split(',')[0].strip())
 					lat = float(coordline.strip().split(',')[1].strip())
 				except ValueError, e:
-					timeline = rpt.readline()
 					continue
 				errline = rpt.readline()
 				timeline = rpt.readline()
 				time = timeline.strip().split(' ')[0]
+				runtype = rpt.readline()
+				rpt.close()
 				csv.write('%s,%.6f,%.6f,%d,%s\n' % (datestr,lat,lon,iguana_num,time))
-			rpt.close()
-		elif f.endswith('.rpt'):
-			rpt = open(os.path.join(dirpath, f), 'r')
-			print(rpt)
-			iguanaline = rpt.readline()
-			iguana_num = int(iguanaline.split()[1])
-			print(iguana_num)
-			coordline = rpt.readline()
-			try:
-				lon = float(coordline.strip().split(',')[0].strip())
-				lat = float(coordline.strip().split(',')[1].strip())
-			except ValueError, e:
-				continue
-			errline = rpt.readline()
-			timeline = rpt.readline()
-			time = timeline.strip().split(' ')[0]
-			runtype = rpt.readline()
-			rpt.close()
-			csv.write('%s,%.6f,%.6f,%d,%s\n' % (datestr,lat,lon,iguana_num,time))
-csv.close()
-
-# rpt = open('2017.08.31.txt', 'r')
-# while True:
-# 	line = rpt.readline()
-# 	if line == "" or line is None:
-# 		break
-# 	if line.startswith('Iguana'):
-# 		iguana_num = int(line.split()[1])
-# 		coordline = rpt.readline()
-# 		try:
-# 			lon = float(coordline.strip().split(',')[0].strip())
-# 			lat = float(coordline.strip().split(',')[1].strip())
-# 		except ValueError, e:
-# 			print(coordline.strip().split(',')[0].strip())
-# 			continue
-# 		errline = rpt.readline()
-# 		timeline = rpt.readline()
-# 		time = timeline.strip().split(' ')[0]
-# 		runtype = rpt.readline()
-# 		csv.write('%s,%.6f,%.6f,%d,%s\n' % (date,lat,lon,iguana_num,time))
-# csv.close()
+	csv.close()
--- utilities/triangulate.py
+++ utilities/triangulate.py
@@ -24,98 +24,93 @@ def bearing_complement(bearing):
 	return retval
 
 def main():
-	pass
+	output_path = '.'
+	iguanaNum = int(raw_input('Iguana Number: '))
+	print("Enter lat lon (x.xxxx,y.yyyy), done when done")
+	latlonstr1 = raw_input('Point 1: ')
+	bearing1 = float(raw_input('Bearing: '))
+	if latlonstr1.lower() == 'done':
+		return
+	latlonstr2 = raw_input('Point 2: ')
+	bearing2 = float(raw_input('Bearing: '))
+	if latlonstr2.lower() == 'done':
+		return
+	latlonstr3 = raw_input('Point 3: ')
+	utm1 = strToGPS(latlonstr1)
+	utm2 = strToGPS(latlonstr2)
+	if latlonstr3.lower() == 'done':
+		# baseline_dist = baseline_distance(utm1, utm2)
+		# baseline_brng = baseline_bearing(utm1, utm2)
+		# alpha = baseline_brng - bearing1
+		# beta = bearing_complement(baseline_brng) - bearing2
+		# d = baseline_dist * (math.sin(math.radians(alpha)) * math.sin(math.radians(beta))) / (math.sin(math.radians(alpha + beta)))
+		m1 = 1 / np.tan(math.radians(bearing1))
+		m2 = 1 / np.tan(math.radians(bearing2))
+		b1 = utm1[1] - m1 * utm1[0]
+		b2 = utm2[1] - m2 * utm2[0]
+		a = np.array([[1, -1 * m1],[1, -1 * m2]])
+		b = np.array([b1, b2])
+		x = np.linalg.solve(a, b)
+		coord = utm.to_latlon(x[1], x[0], utm1[2], utm1[3])
+		print('%.6f, %.6f' % (coord[0], coord[1]))
+		rpt = open('%s/CH_%06d.rpt' % (output_path, iguanaNum), 'w+')
+		rpt.write('Iguana %d\n' % iguanaNum)
+		rpt.write('\t%.6f, %.6f\n' % (coord[0], coord[1]))
+		rpt.write('\tUnknown Error - 2 measurements\n')
+		est_time = raw_input('Time: ')
+		rpt.write('\t%s Local\n' % est_time)
+		rpt.close()
+	else:
+		bearing3 = float(raw_input('Bearing: '))
+		utm3 = strToGPS(latlonstr3)
+		m1 = 1 / np.tan(math.radians(bearing1))
+		m2 = 1 / np.tan(math.radians(bearing2))
+		m3 = 1 / np.tan(math.radians(bearing3))
+		b1 = utm1[1] - m1 * utm1[0]
+		b2 = utm2[1] - m2 * utm2[0]
+		b3 = utm3[1] - m3 * utm3[0]
+		a = np.array([[1, -1 * m1],[1, -1 * m2], [1, -1 * m3]])
+		b = np.array([b1, b2, b3])
+		x = np.linalg.lstsq(a, b)
+		coord = utm.to_latlon(x[0][1], x[0][0], utm1[2], utm1[3])
+
+		a = np.array([[1, -m1],[1,-m2]])
+		b = np.array([b1, b2])
+		x12 = np.linalg.solve(a, b)
+		a = np.array([[1, -m1], [1, -m3]])
+		b = np.array([b1, b3])
+		x13 = np.linalg.solve(a, b)
+		a = np.array([[1, -m2], [1, -m3]])
+		b = np.array([b2, b3])
+		x23 = np.linalg.solve(a, b)
+		err1 = baseline_distance(x[0], x12)
+		err2 = baseline_distance(x[0], x13)
+		err3 = baseline_distance(x[0], x23)
+		maxErr = max([err1, err2, err3])
+		print('%.6f, %.6f +/- %d m' % (coord[0], coord[1], math.ceil(maxErr)))
+
+		rpt = open('%s/CH_%06d.rpt' % (output_path, iguanaNum), 'w+')
+		rpt.write('Iguana %d\n' % iguanaNum)
+		rpt.write('\t%.6f, %.6f\n' % (coord[1], coord[0]))
+		rpt.write('\t+/- %d m\n' % math.ceil(maxErr))
+		est_time = raw_input('Time: ')
+		rpt.write('\t%s Local\n' % est_time)
+		rpt.write('\tTriangulation\n')
+		rpt.close()
+
+	w = shapefile.Writer(shapefile.POINT)
+	w.autoBalance = 1
+	w.field("channel", 'N')
+
+	w.point(coord[1], coord[0])
+	w.record(iguanaNum)
+	w.save('%s/CH_%06d_est.shp' % (output_path, iguanaNum))
+
+	prj = open('%s/CH_%06d_est.prj' % (output_path, iguanaNum), "w")
+	epsg = 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433]]'
+	prj.write(epsg)
+	prj.close()
 
 
 if __name__ == '__main__':
 	main()
-
-iguanaNum = int(raw_input('Iguana Number: '))
-print("Enter lat lon (x.xxxx,y.yyyy), done when done")
-latlonstr1 = raw_input('Point 1: ')
-bearing1 = float(raw_input('Bearing: '))
-if latlonstr1.lower() == 'done':
-	exit()
-latlonstr2 = raw_input('Point 2: ')
-bearing2 = float(raw_input('Bearing: '))
-if latlonstr2.lower() == 'done':
-	exit()
-latlonstr3 = raw_input('Point 3: ')
-utm1 = strToGPS(latlonstr1)
-utm2 = strToGPS(latlonstr2)
-output_path = '.'
-if latlonstr3.lower() == 'done':
-	# baseline_dist = baseline_distance(utm1, utm2)
-	# baseline_brng = baseline_bearing(utm1, utm2)
-	# alpha = baseline_brng - bearing1
-	# beta = bearing_complement(baseline_brng) - bearing2
-	# d = baseline_dist * (math.sin(math.radians(alpha)) * math.sin(math.radians(beta))) / (math.sin(math.radians(alpha + beta)))
-	m1 = 1 / np.tan(math.radians(bearing1))
-	m2 = 1 / np.tan(math.radians(bearing2))
-	b1 = utm1[1] - m1 * utm1[0]
-	b2 = utm2[1] - m2 * utm2[0]
-	a = np.array([[1, -1 * m1],[1, -1 * m2]])
-	b = np.array([b1, b2])
-	x = np.linalg.solve(a, b)
-	coord = utm.to_latlon(x[1], x[0], utm1[2], utm1[3])
-	print('%.6f, %.6f' % (coord[0], coord[1]))
-	rpt = open('%s/CH_%06d.rpt' % (output_path, iguanaNum), 'w+')
-	rpt.write('Iguana %d\n' % iguanaNum)
-	rpt.write('\t%.6f, %.6f\n' % (coord[0], coord[1]))
-	rpt.write('\tUnknown Error - 2 measurements\n')
-	est_time = raw_input('Time: ')
-	rpt.write('\t%s Local\n' % est_time)
-	rpt.close()
-else:
-	bearing3 = float(raw_input('Bearing: '))
-	utm3 = strToGPS(latlonstr3)
-	m1 = 1 / np.tan(math.radians(bearing1))
-	m2 = 1 / np.tan(math.radians(bearing2))
-	m3 = 1 / np.tan(math.radians(bearing3))
-	b1 = utm1[1] - m1 * utm1[0]
-	b2 = utm2[1] - m2 * utm2[0]
-	b3 = utm3[1] - m3 * utm3[0]
-	a = np.array([[1, -1 * m1],[1, -1 * m2], [1, -1 * m3]])
-	b = np.array([b1, b2, b3])
-	x = np.linalg.lstsq(a, b)
-	coord = utm.to_latlon(x[0][1], x[0][0], utm1[2], utm1[3])
-
-	a = np.array([[1, -m1],[1,-m2]])
-	b = np.array([b1, b2])
-	x12 = np.linalg.solve(a, b)
-	a = np.array([[1, -m1], [1, -m3]])
-	b = np.array([b1, b3])
-	x13 = np.linalg.solve(a, b)
-	a = np.array([[1, -m2], [1, -m3]])
-	b = np.array([b2, b3])
-	x23 = np.linalg.solve(a, b)
-	err1 = baseline_distance(x[0], x12)
-	err2 = baseline_distance(x[0], x13)
-	err3 = baseline_distance(x[0], x23)
-	maxErr = max([err1, err2, err3])
-	print('%.6f, %.6f +/- %d m' % (coord[0], coord[1], math.ceil(maxErr)))
-
-	rpt = open('%s/CH_%06d.rpt' % (output_path, iguanaNum), 'w+')
-	rpt.write('Iguana %d\n' % iguanaNum)
-	rpt.write('\t%.6f, %.6f\n' % (coord[1], coord[0]))
-	rpt.write('\t+/- %d m\n' % math.ceil(maxErr))
-	est_time = raw_input('Time: ')
-	rpt.write('\t%s Local\n' % est_time)
-	rpt.write('\tTriangulation\n')
-	rpt.close()
-
-
-
-
-w = shapefile.Writer(shapefile.POINT)
-w.autoBalance = 1
-w.field("channel", 'N')
-
-w.point(coord[1], coord[0])
-w.record(iguanaNum)
-w.save('%s/CH_%06d_est.shp' % (output_path, iguanaNum))
-
-prj = open('%s/CH_%06d_est.prj' % (output_path, iguanaNum), "w")
-epsg = 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433]]'
-prj.write(epsg)
-prj.close()
