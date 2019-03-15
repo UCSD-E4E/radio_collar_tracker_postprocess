79fc5cad56d30b326e22081ecc7fd7a261f79d12
Nathan Hui <ntlhui@gmail.com>
2019-03-14 20:29:51 -0700

    Functionized script

--- matlab/waterfall.m
+++ matlab/waterfall.m
@@ -1,21 +1,20 @@
-close all;
-clear all;
-
-FFT_LEN = 4096;
-CENTER_FREQ = 172250000;
-data = raw2complex('/tmp/sdr_data/RAW_DATA_000001_000001');
-
-data_len = length(data);
-num_ffts = data_len / FFT_LEN;
-fft_in_data = reshape(data, [FFT_LEN, num_ffts]);
-fft_out_data = fft(fft_in_data);
-P2 = abs(fft_out_data / FFT_LEN);
-P2 = [P2(FFT_LEN/2+1:FFT_LEN,:); P2(1:FFT_LEN/2,:)];
-waterfall_plot = 10*log10(abs(P2))';
-figure;
-imagesc([(0 + CENTER_FREQ) / 1e6, (1e6 + CENTER_FREQ) / 1e6], [0, data_len / 2000000], waterfall_plot(:,FFT_LEN/2+1:FFT_LEN), [max(std(waterfall_plot)) + mean(mean(waterfall_plot)), max(max(waterfall_plot))]);
-c = colorbar;
-c.Label.String = 'Signal Strength (dBFS)'
-ylabel('Time (s)');
-xlabel('Frequency (MHz)');
-title('Signal Waterfall Plot');
+function waterfall(fft_len, center_freq, samp_freq, data)
+	data_len = length(data);
+	num_ffts = floor(data_len / fft_len);
+	fft_in_data = reshape(data(1:num_ffts * fft_len), [fft_len, num_ffts]);
+	fft_out_data = fft(fft_in_data);
+	shifted_amp_data = abs(fft_out_data / fft_len);
+	amp_data = [shifted_amp_data(fft_len/2+1:fft_len,:); shifted_amp_data(1:fft_len/2,:)];
+	% amp_data = [shifted_amp_data(fft_len / 2 + 1 : fft_len, :); ...
+	% 	shifted_amp_data(1 : fft_len / 2, :)];
+	power_data = fftshift(10 * log10(abs(amp_data))',2);
+	fig = figure;
+	freq_bound = samp_freq / 2;
+	imagesc([(-freq_bound + center_freq) / 1e6, (freq_bound + center_freq) / 1e6], ...
+		[0, data_len / samp_freq], power_data, ...
+		[mean(std(power_data)) + mean(mean(power_data)), max(max(power_data))]);
+	c = colorbar;
+	c.Label.String = 'Signal Strength (dBFS)';
+	ylabel('Time (s)');
+	xlabel('Frequency (MHz)');
+	title('Signal Waterfall Plot');
